6a0af24c568023814895347aeb8ba96d357aa96f
EzzioMoreira <enzziom@gmail.com>
2022-03-30 19:37:23 -0300

    feat: add variables task json

--- how-to-use-this-module/terrafile.tf
+++ how-to-use-this-module/terrafile.tf
@@ -1,6 +1,19 @@
+provider "aws" {
+  region  = "us-east-2"
+  version = "~> 4.0"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "NOME-BUCKET-S3"
+    key    = "path/NOME-APP-terraform-.tfstate"
+    region = "us-east-2"
+  }
+} 
+
 module "ecs_mentoria" {
   source             = "../"
-  app_port           = 80
+  app_port           = var.APP_PORT
   region             = "us-east-2"
   app_count          = 1
   fargate_cpu        = 256
@@ -11,12 +24,45 @@ module "ecs_mentoria" {
   protocol           = "HTTP"
   policy_ssl         = null
   certificate_arn    = null
-  family_name        = "mentoria-iac"
-  service_name       = "mentoria-iac"
-  cluster_name       = "mentoria-iac"
+  family_name        = var.APP_NAME
+  service_name       = var.APP_NAME
+  cluster_name       = var.APP_NAME
   template_container = "task-definitions.json"
   tags = {
     name = "cluster-ecs-mentoria-iac"
   }
   
+}
+
+output "load_balancer_dns_name" {
+  value = "http://${module.ecs_mentoria.loadbalance_dns_name}"
+}
+
+data "template_file" "task-definitions_json" {
+  template = file("./task-definitions.json")
+
+  vars = {
+    APP_VERSION = var.APP_VERSION
+    APP_IMAGE   = var.APP_IMAGE
+  }
+}
+
+variable "APP_NAME" {
+  default   = "mentoria_iac"
+  description = "Nome da aplicação"
+}
+
+variable "APP_IMAGE" {
+  default   = "httpd"
+  description = "Endereço do registre docker da imagem"
+}
+
+variable "APP_VERSION" {
+  default   = "alpine3.15"
+  description = "Versão da imagem docker"
+}
+
+variable "APP_PORT" {
+  default   = 80
+  description = "Porta do container"
 }
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "loadbalance_dns_name" {
+  value = aws_alb.iac_lb.dns_name
+}
